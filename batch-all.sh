a0a485878e1a7760d35d11b14f84aa706d86301a
zerrymore11 <zerrymore@gmail.com>
2025-02-15 22:00:28 +0800

    Update scripts

--- batch-all.sh
+++ batch-all.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 ./batch-run.sh Executability  --derivcheck-timeout=0  -D=exec --prove=Executability \
---prove=SourcesID 
+--prove=SourcesID --verbose
 ./batch-run.sh Goal1 --derivcheck-timeout=0 -D=goal1 --prove=SanityCheckCodeSecrcy \
 --prove=SanityCheckTokensecrcy \
---prove=SourcesID \
---prove=CodeSecrecy \
 --prove=TokenSecrecy \
---prove=ClaimsSecrecy \
---prove=secrecyOfCredential
-./batch-run.sh Goal2  --derivcheck-timeout=0  -D=goal2 --prove=NonInjectAuthVCIssurance \
---prove=integrityOfVC \
---prove=SourcesID 
-./batch-run.sh Goal3 --derivcheck-timeout=0 -D=goal3 --prove=UniquePass \
---prove=SourcesID 
-./batch-run.sh Goal4 --derivcheck-timeout=0 -D=goal4 --prove=LegalPresentation \
---prove=SourcesID 
-./batch-run.sh Goal5 --derivcheck-timeout=0 -D=goal5 --prove=SelectiveDisclosure \
+--prove=CodeSecrecy \
 --prove=SourcesID \
---prove=StrictSelectiveDisclosure
+--prove=userinfoSecrecy \
+--prove=claimsSecrecy \
+--prove=VCSecrecy --verbose
+./batch-run.sh Goal2  --derivcheck-timeout=0  -D=goal2 \
+--prove=VCIntegrity \
+--prove=SourcesID --verbose
+./batch-run.sh Goal3 --derivcheck-timeout=0 -D=goal3 --prove=UniquePass \
+--prove=SourcesID --verbose
+./batch-run.sh Goal4 --derivcheck-timeout=0 -D=goal4 --prove=LegalPresentation \
+--prove=SourcesID --verbose
+./batch-run.sh Goal5 --derivcheck-timeout=0 -D=goal5 --prove=weakest_SelectiveDisclosure \
+--prove=SourcesID \
+--prove=sd_auth_storeVC \
+--prove=auxlemma \
+--prove=sd_auth_issueVC --verbose
--- batch-run.sh
+++ batch-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tamarin-prover +RTS -N24 -RTS --Output=proof --output=$1.spthy "${@:2}" DIDSuperApp.spthy > $1.log 2>&1
+tamarin-prover +RTS -N24 -RTS --Output=proof --output=./proof/$1.spthy "${@:2}" DIDModel.spthy
